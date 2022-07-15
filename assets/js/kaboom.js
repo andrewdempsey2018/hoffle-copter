f88ef0c191c778e8e3051e44f778baa5e35a695a
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-07-15 10:48:20 +0100

    Game canvas centered in browser

--- assets/js/kaboom.js
+++ assets/js/kaboom.js
@@ -1,11 +1,13 @@
-// import kaboom lib
+// import kaboom lib from CDN
 import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
 
 // initialize kaboom context
 export const k = kaboom({
     width: 1024,
     height: 768,
-    background: [255, 255, 255] //white
+    background: [255, 255, 255], // white
+    canvas: document.querySelector("game_canvas") // gameplay is drawn to canvas
 });
 
+// make kaboom available in other scripts
 export default k;
