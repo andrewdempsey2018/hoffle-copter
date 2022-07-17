6afccb5154dd29fe1eb73d0104dc478dcf748c62
ccp84 <ccp84@icloud.com>
2022-07-17 18:01:06 +0100

    add animation code for explosion

--- assets/js/explode.js
+++ assets/js/explode.js
@@ -1,6 +1,17 @@
 import k from "./kaboom.js"
 
-loadSprite("boom", "./assets/sprites/boom.png");
+loadSprite("boom", "./assets/sprites/boom.png", {
+    sliceY: 3,
+    //Animation of explosion
+    anims: {
+        "xplod": {
+            from: 0,
+            to: 3,
+            speed: 1,
+            loop: false
+        }
+    }
+})
 
 class boom {
 
@@ -14,7 +25,9 @@ class boom {
             k.area(),
             k.pos(this.xPos, this.yPos),
             "boom"
-        ]);
+        ]); 
+
+        this.spr.play("xplod");
     }
 
     animate() {
