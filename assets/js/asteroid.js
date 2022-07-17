73a57b394623d193ff638095510468f5c2a402c0
Lucas Behrendt <lu.behrendt@hotmail.com>
2022-07-17 02:08:28 +0200

    Add asteroid enemy type

---
+++ assets/js/asteroid.js
@@ -0,0 +1,31 @@
+import k from "./kaboom.js"
+
+loadSprite("asteroid", "./assets/sprites/asteroid.png");
+
+class asteroid {
+
+    constructor(xPos, yPos, speed) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.speed = speed;
+        this.angle = 0;
+
+        this.spr = k.add([
+            k.sprite("asteroid"),
+            k.area(),
+            k.rotate(this.angle),
+            k.origin('center'),
+            k.scale(0.2),
+            k.pos(this.xPos, this.yPos),
+            "asteroid"
+        ]);
+    }
+
+
+    move() {
+        this.spr.move(this.speed, 0);
+        this.spr.angle++
+    };
+}
+
+export default asteroid;
