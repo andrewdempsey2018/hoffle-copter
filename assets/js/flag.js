a45dde9c503f509a97e81e4287603ab915a21cf2
Lucas Behrendt <lu.behrendt@hotmail.com>
2022-07-18 01:41:23 +0200

    Update beach level & add flag scripts in main

--- assets/js/flag.js
+++ assets/js/flag.js
@@ -15,20 +15,25 @@ loadSprite("flag", "./assets/sprites/flag.png", {
 
 class flag {
 
-    constructor(xPos, yPos) {
+    constructor(xPos, yPos, speed) {
         this.xPos = xPos;
         this.yPos = yPos;
+        this.speed = speed;
         this.life = 50;
 
         this.spr = k.add([
             k.sprite("flag"),
             k.area(),
+            k.scale(0.5),
             k.pos(this.xPos, this.yPos),
             "flag"
         ]); 
 
         this.spr.play("wave");
     }
+    move() {
+        this.spr.move(this.speed, 0);
+    };
 
 }
 
