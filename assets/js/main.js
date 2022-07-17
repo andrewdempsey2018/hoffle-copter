0aec3d5a593ad7003812b51095d0db0c5fc0b11b
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-07-17 13:09:45 +0100

    added test scenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,22 +31,15 @@ let blimpColl = new Set();
 let planeColl = new Set();
 let saucerColl = new Set();
 let asteroidColl = new Set();
-//collectable game object 
 let collectableColl = new Set();
+
+/* Create players bullet collection and handle
+player controls to allow shooting */
+let bullets = new Set();
 
 /* Sprite assets loaded here */
 
 const HELI_SPEED = 300;
-
-const heli = add([
-    sprite("heli"),
-    area(),
-    z(1),
-    pos(400, 300),
-    "heli"
-]);
-
-heli.play("fly");
 
 /* Load sound effects */
 loadSound("shoot", "./assets/sfx/shoot.wav");
@@ -55,171 +48,204 @@ loadSound("explosion2", "./assets/sfx/explosion2.wav");
 loadSound("coin", "./assets/sfx/coin.wav");
 loadSound("bgmus", "./assets/soundfile/roflcopter-sound.wav");
 
-/* Setup control scheme for player */
-onKeyDown("up", () => {
-    if (heli.pos.y > 0) {
-        heli.move(0, -HELI_SPEED);
-    }
-});
+scene("gameplay", async ({ levelIdx, score }) => {
 
-onKeyDown("down", () => {
-    heli.move(0, HELI_SPEED);
-});
+    const heli = add([
+        sprite("heli"),
+        area(),
+        z(1),
+        pos(400, 300),
+        "heli"
+    ]);
 
-onKeyDown("left", () => {
-    if (heli.pos.x > 0) {
-        heli.move(-HELI_SPEED, 0);
-    }
-});
+    heli.play("fly");
 
-onKeyDown("right", () => {
-    heli.move(HELI_SPEED, 0);
-});
-
-//set up the game screen area
-onKeyPress("f", () => {
-    fullscreen(!isFullscreen())
-});
-
-/* Create players bullet collection and handle
-player controls to allow shooting */
-let bullets = new Set();
-
-keyPress("z", () => {
-    bullets.add(new bullet(heli.screenPos().x + 170, heli.screenPos().y + 70, 900));
-    play("shoot");
-});
-
-/* Collision between bullets and enemys */
-
-onCollide("bullet", "plane", (bullet, plane) => {
-    play("explosion2");
-    bullet.destroy();
-    plane.destroy();
-});
-
-onCollide("heli", "copper", (heli, copper) => {
-    play("coin");
-    destroy(copper);
-});
-
-// City Skyline
-onCollide("bullet", "blimp", (bullet, blimp) => {
-    play("explosion2");
-    bullet.destroy();
-    blimp.destroy();
-});
-
-/* Here we read each entry from the level JSON file
-every one second. If the level file contains an entry 'no spawn' we ignore it
-If the level file contains information on a game object, we instanciate it
-using the values from the JSON file */
-
-let level2 = await loadLevel('./assets/levels/testlev.json'); //grab the level from assets folder
-
-/* index is the position in the level script where we are at. 
-There is a position for every second of real time that passes.
-For now, limited to 60 seconds */
-let index = -1;
-
-/* JSON representation of a game object.
-We use this JSON data to pass into the actual game object constructors */
-let gameObject = null; 
-
-const LEVEL_TIME_SECONDS = 60; //make sure the event timer does not look for out of bounds JSON data
-
-loop(1, () => {
-
-    if (index < LEVEL_TIME_SECONDS) {
-        index++;
-    }
-
-    gameObject = level2[index];
-
-    if (gameObject.object === "blimp") {
-        blimpColl.add(new blimp(gameObject.x, gameObject.y, gameObject.xSpeed, gameObject.ySpeed));
-    }
-
-    if (gameObject.object === "cloud") {
-        cloudColl.add(new cloud(gameObject.x, gameObject.y, gameObject.speed, gameObject.sized));
-    }
-
-    if (gameObject.object === "plane") {
-        planeColl.add(new plane(gameObject.x, gameObject.y, gameObject.speed));
-    }
-
-    if (gameObject.object === "copper") {
-        collectableColl.add(new collectable(gameObject.x, gameObject.y, gameObject.speed));
-    }
-
-    if (gameObject.object === "cityScape") {
-        cityScapeColl.add(new cityScape(gameObject.x, gameObject.y, gameObject.speed));
-    }
-
-    if (gameObject.object === "asteroid") {
-        asteroidColl.add(new asteroid(gameObject.x, gameObject.y, gameObject.speed));
-    }
-
-    if (gameObject.object === "saucer") {
-        saucerColl.add(new saucer(gameObject.x, gameObject.y, gameObject.speed));
-    }
-});
-
-onUpdate(() => {
-
-    blimpColl.forEach(blimp => {
-        blimp.move();
+    /* Setup control scheme for player */
+    onKeyDown("up", () => {
+        if (heli.pos.y > 0) {
+            heli.move(0, -HELI_SPEED);
+        }
     });
 
-    bullets.forEach(bullet => {
-        bullet.move();
+    onKeyDown("down", () => {
+        heli.move(0, HELI_SPEED);
     });
 
-    // Moving Cityscape 
-    cityScapeColl.forEach(cityScape => {
-        cityScape.move();
+    onKeyDown("left", () => {
+        if (heli.pos.x > 0) {
+            heli.move(-HELI_SPEED, 0);
+        }
     });
 
-    // Moving Clouds in background
-    cloudColl.forEach(cloud => {
-        cloud.move();
+    onKeyDown("right", () => {
+        heli.move(HELI_SPEED, 0);
     });
 
-    //Moving copper game object
-    collectableColl.forEach(collectable => {
-        collectable.move()
-    })
-
-    // Moving plane
-    planeColl.forEach(plane => {
-        plane.move();
+    //set up the game screen area
+    onKeyPress("f", () => {
+        fullscreen(!isFullscreen())
     });
 
-    // Moving asteroid
-    asteroidColl.forEach(asteroid => {
-        asteroid.move();
+    keyPress("z", () => {
+        bullets.add(new bullet(heli.screenPos().x + 170, heli.screenPos().y + 70, 900));
+        play("shoot");
     });
 
-    // Moving saucer
-    saucerColl.forEach(saucer => {
-        saucer.move();
-    });
-    
-    // Looping background music
-    const music = play("bgmus", {
-        volume: 0.8,
-        loop: true   
-   
-    })
+    /* Collision between bullets and enemys */
 
-    // User controls for music play pause
-    onKeyPress("space", () => {
-        if (music.isPaused()) {
-            music.play()
-        } else {
-            music.pause()
+    onCollide("bullet", "plane", (bullet, plane) => {
+        play("explosion2");
+        bullet.destroy();
+        plane.destroy();
+    });
+
+    onCollide("heli", "copper", (heli, copper) => {
+        play("coin");
+        destroy(copper);
+    });
+
+    // City Skyline
+    onCollide("bullet", "blimp", (bullet, blimp) => {
+        play("explosion2");
+        bullet.destroy();
+        blimp.destroy();
+    });
+
+    /* Here we read each entry from the level JSON file
+    every one second. If the level file contains an entry 'no spawn' we ignore it
+    If the level file contains information on a game object, we instanciate it
+    using the values from the JSON file */
+
+    let level2 = await loadLevel('./assets/levels/testlev.json'); //grab the level from assets folder
+
+    /* index is the position in the level script where we are at. 
+    There is a position for every second of real time that passes.
+    For now, limited to 60 seconds */
+    let index = -1;
+
+    /* JSON representation of a game object.
+    We use this JSON data to pass into the actual game object constructors */
+    let gameObject = null;
+
+    const LEVEL_TIME_SECONDS = 60; //make sure the event timer does not look for out of bounds JSON data
+
+    loop(1, () => {
+
+        if (index < LEVEL_TIME_SECONDS) {
+            index++;
+        }
+
+        gameObject = level2[index];
+
+        if (gameObject.object === "blimp") {
+            blimpColl.add(new blimp(gameObject.x, gameObject.y, gameObject.xSpeed, gameObject.ySpeed));
+        }
+
+        if (gameObject.object === "cloud") {
+            cloudColl.add(new cloud(gameObject.x, gameObject.y, gameObject.speed, gameObject.sized));
+        }
+
+        if (gameObject.object === "plane") {
+            planeColl.add(new plane(gameObject.x, gameObject.y, gameObject.speed));
+        }
+
+        if (gameObject.object === "copper") {
+            collectableColl.add(new collectable(gameObject.x, gameObject.y, gameObject.speed));
+        }
+
+        if (gameObject.object === "cityScape") {
+            cityScapeColl.add(new cityScape(gameObject.x, gameObject.y, gameObject.speed));
+        }
+
+        if (gameObject.object === "asteroid") {
+            asteroidColl.add(new asteroid(gameObject.x, gameObject.y, gameObject.speed));
+        }
+
+        if (gameObject.object === "saucer") {
+            saucerColl.add(new saucer(gameObject.x, gameObject.y, gameObject.speed));
+        }
+    });
+
+    onUpdate(() => {
+
+        blimpColl.forEach(blimp => {
+            blimp.move();
+        });
+
+        bullets.forEach(bullet => {
+            bullet.move();
+        });
+
+        // Moving Cityscape 
+        cityScapeColl.forEach(cityScape => {
+            cityScape.move();
+        });
+
+        // Moving Clouds in background
+        cloudColl.forEach(cloud => {
+            cloud.move();
+        });
+
+        //Moving copper game object
+        collectableColl.forEach(collectable => {
+            collectable.move()
+        })
+
+        // Moving plane
+        planeColl.forEach(plane => {
+            plane.move();
+        });
+
+        // Moving asteroid
+        asteroidColl.forEach(asteroid => {
+            asteroid.move();
+        });
+
+        // Moving saucer
+        saucerColl.forEach(saucer => {
+            saucer.move();
+        });
+
+        /* Looping background music
+        const music = play("bgmus", {
+            volume: 0.8,
+            loop: true
+
+        }) */
+
+        // User controls for music play pause
+        onKeyPress("space", () => {
+            if (music.isPaused()) {
+                music.play()
+            } else {
+                music.pause()
             }
-    })
+        })
 
+    });
 });
-    
+
+
+
+const level1Start = () => {
+    go("gameplay", {
+        levelIdx: 0,
+        score: 0,
+    })
+}
+
+scene("titleScreen", async ({ levelIdx, score }) => {
+
+    onKeyPress("space", () => {
+        level1Start();
+    })
+});
+
+const titleScreenStart = () => {
+    go("titleScreen", {
+        levelIdx: 0,
+        score: 0,
+    })
+}
+
+titleScreenStart();
