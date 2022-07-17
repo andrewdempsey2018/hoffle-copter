d2410e2cdab5e327909bf54f028f5d894b404dac
Christian Brown <robrowno@icloud.com>
2022-07-17 19:13:20 +0100

    Add replica js files for beach and planet levels, add ascii and beach scrollingpng files

---
+++ assets/js/beach.js
@@ -0,0 +1,27 @@
+import k from "./kaboom.js"
+
+loadSprite("planetScape", "./assets/sprites/beach.png");
+
+class planetScape {
+
+    constructor(xPos, yPos, speed) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.speed = speed;
+
+        this.spr = k.add([
+            k.sprite("planetScape"),
+            k.area(),
+            k.pos(this.xPos, this.yPos),
+            "planetScape"
+        ]);
+    }
+
+    move() {
+        this.spr.move(this.speed, 0);
+
+    };
+}
+
+export default planetScape;
+
