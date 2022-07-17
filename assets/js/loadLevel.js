d550f165297d996bcebfa78a031608c43a977da9
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-07-17 08:53:22 +0100

    refactor level load function

--- assets/js/loadLevel.js
+++ assets/js/loadLevel.js
@@ -1,5 +1,5 @@
 const loadLevel = async (level) => {
-    const levelData = await fetch('./assets/levels/'+ level + '.json');
+    const levelData = await fetch(level);
     return levelData.json();
 }
 
